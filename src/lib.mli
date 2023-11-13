75b8f9886e9201fdbea224556489906f7a36afb0
Jianwei Chen <jchen362@jhu.edu>
2023-11-13 13:53:54 -0500

    begining board state

--- src/lib.mli
+++ src/lib.mli
@@ -1,4 +1,24 @@
-type piece_type = Pawn | Rook | Knight | Queen | King
+open Core
+
+
+type piece_type = Pawn | Rook | Knight | Queen | King | Bishop
+type color = Black | White
+
+
+type position_key = {x: int; y: int} [@@deriving compare, sexp]
+
+module Board_State: sig
+    module Item_Key : Map.Key with type t = position_key
+    type t
+    val import: string -> t
+    val export: t -> string
+    val in_check: t -> color -> bool
+    val in_checkmate: t -> color -> bool 
+    val in_stalemate: t -> color -> bool
+
+
+end
+
 
 module type Piece = 
     sig
