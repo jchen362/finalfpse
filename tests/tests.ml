bd0c0036fa87b73a345576257651e9ef7e6c2fb8
Brandon Wong <bwong19@jhu.edu>
2023-12-05 22:06:58 -0500

    added can_move and move_invariant tests

--- tests/tests.ml
+++ tests/tests.ml
@@ -2,6 +2,7 @@ open Core
 open OUnit2
 open Lib
 
+[@@@ocaml.warning "-32"]
 (* type position_key = {x : int; y : int} [@@deriving compare, sexp] *)
 
 let pos_in = { x = 3; y = 4 }
@@ -50,12 +51,186 @@ let test_out_bounds_y2 _ =
   assert_equal false @@ Queen.in_bounds pos_out_y2;
   assert_equal false @@ King.in_bounds pos_out_y2
 
-let test_pawn _ = assert_equal true true
-let test_bishop _ = assert_equal true true
-let test_knight _ = assert_equal true true
-let test_rook _ = assert_equal true true
-let test_queen _ = assert_equal true true
-let test_king _ = assert_equal true true
+let default_pos_white_pawn = { x = 4; y = 1 }
+let start_pos = { x = 4; y = 4 }
+
+let end_pos_vert =
+  [
+    { x = 4; y = 0 };
+    { x = 4; y = 1 };
+    { x = 4; y = 2 };
+    { x = 4; y = 3 };
+    { x = 4; y = 5 };
+    { x = 4; y = 6 };
+    { x = 4; y = 7 };
+  ]
+
+let end_pos_horiz =
+  [
+    { x = 0; y = 4 };
+    { x = 1; y = 4 };
+    { x = 2; y = 4 };
+    { x = 3; y = 4 };
+    { x = 5; y = 4 };
+    { x = 6; y = 4 };
+    { x = 7; y = 4 };
+  ]
+
+let end_pos_diag_positive =
+  [
+    { x = 0; y = 0 };
+    { x = 1; y = 1 };
+    { x = 2; y = 2 };
+    { x = 3; y = 3 };
+    { x = 5; y = 5 };
+    { x = 6; y = 6 };
+    { x = 7; y = 7 };
+  ]
+
+let end_pos_diag_negative =
+  [
+    { x = 1; y = 7 };
+    { x = 2; y = 6 };
+    { x = 3; y = 5 };
+    { x = 5; y = 3 };
+    { x = 6; y = 2 };
+    { x = 7; y = 1 };
+  ]
+
+let end_pos_default_white_pawn =
+  [ { x = 3; y = 2 }; { x = 4; y = 2 }; { x = 5; y = 2 }; { x = 4; y = 3 } ]
+
+let end_pos_white_pawn =
+  [ { x = 3; y = 5 }; { x = 4; y = 5 }; { x = 5; y = 5 } ]
+
+let end_pos_knight =
+  [
+    { x = 2; y = 3 };
+    { x = 6; y = 3 };
+    { x = 2; y = 5 };
+    { x = 6; y = 5 };
+    { x = 3; y = 2 };
+    { x = 3; y = 6 };
+    { x = 5; y = 2 };
+    { x = 5; y = 6 };
+  ]
+
+let end_pos_king =
+  [
+    { x = 3; y = 3 };
+    { x = 4; y = 3 };
+    { x = 5; y = 3 };
+    { x = 3; y = 4 };
+    { x = 5; y = 4 };
+    { x = 3; y = 5 };
+    { x = 4; y = 5 };
+    { x = 5; y = 5 };
+  ]
+
+let test_pawn_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Pawn.can_move start_pos end_pos White)
+       end_pos_white_pawn;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Pawn.can_move default_pos_white_pawn end_pos White)
+       end_pos_default_white_pawn
+
+let test_pawn_generate_moves _ = assert_equal true true
+
+let test_pawn_move_invariant _ =
+  assert_equal true
+  @@ (Pawn.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> Pawn.can_move start_pos end_pos White))
+
+let test_bishop_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Bishop.can_move start_pos end_pos White)
+       end_pos_diag_positive;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Bishop.can_move start_pos end_pos White)
+       end_pos_diag_negative
+
+let test_bishop_generate_moves _ = assert_equal true true
+
+let test_bishop_move_invariant _ =
+  assert_equal true
+  @@ (Bishop.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> Bishop.can_move start_pos end_pos White)
+     )
+
+let test_knight_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Knight.can_move start_pos end_pos White)
+       end_pos_knight
+
+let test_knight_generate_moves _ = assert_equal true true
+
+let test_knight_move_invariant _ =
+  assert_equal true
+  @@ (Knight.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> Knight.can_move start_pos end_pos White)
+     )
+
+let test_rook_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Rook.can_move start_pos end_pos White)
+       end_pos_horiz;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Rook.can_move start_pos end_pos White)
+       end_pos_vert
+
+let test_rook_generate_moves _ = assert_equal true true
+
+let test_rook_move_invariant _ =
+  assert_equal true
+  @@ (Rook.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> Rook.can_move start_pos end_pos White))
+
+let test_queen_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Queen.can_move start_pos end_pos White)
+       end_pos_diag_positive;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Queen.can_move start_pos end_pos White)
+       end_pos_diag_negative;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Queen.can_move start_pos end_pos White)
+       end_pos_horiz;
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> Queen.can_move start_pos end_pos White)
+       end_pos_vert
+
+let test_queen_generate_moves _ = assert_equal true true
+
+let test_queen_move_invariant _ =
+  assert_equal true
+  @@ (Queen.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> Queen.can_move start_pos end_pos White)
+     )
+
+let test_king_can_move _ =
+  assert_equal true
+  @@ List.for_all
+       ~f:(fun end_pos -> King.can_move start_pos end_pos White)
+       end_pos_king
+
+let test_king_generate_moves _ = assert_equal true true
+
+let test_king_move_invariant _ =
+  assert_equal true
+  @@ (King.generate_moves start_pos White
+     |> List.for_all ~f:(fun end_pos -> King.can_move start_pos end_pos White))
 
 let piece_tests =
   "piece tests"
@@ -66,12 +241,24 @@ let piece_tests =
          "test_out_bounds_x2" >:: test_out_bounds_x2;
          "test_out_bounds_y1" >:: test_out_bounds_y1;
          "test_out_bounds_y2" >:: test_out_bounds_y2;
-         "test_pawn" >:: test_pawn;
-         "test_bishop" >:: test_bishop;
-         "test_knight" >:: test_knight;
-         "test_rook" >:: test_rook;
-         "test_queen" >:: test_queen;
-         "test_king" >:: test_king;
+         "test_pawn_can_move" >:: test_pawn_can_move;
+         (* "test_pawn_generate_moves" >:: test_pawn_generate_moves; *)
+         (* "test_pawn_move_invariant" >:: test_pawn_move_invariant; *)
+         "test_bishop_can_move" >:: test_bishop_can_move;
+         (* "test_bishop_generate_moves" >:: test_bishop_generate_moves; *)
+         (* "test_bishop_move_invariant" >:: test_bishop_move_invariant; *)
+         "test_knight_can_move" >:: test_knight_can_move;
+         (* "test_knight_generate_moves" >:: test_knight_generate_moves; *)
+         (* "test_knight_move_invariant" >:: test_knight_move_invariant; *)
+         "test_rook_can_move" >:: test_rook_can_move;
+         (* "test_rook_generate_moves" >:: test_rook_generate_moves; *)
+         (* "test_rook_move_invariant" >:: test_rook_move_invariant; *)
+         "test_queen_can_move" >:: test_queen_can_move;
+         (* "test_queen_generate_moves" >:: test_queen_generate_moves; *)
+         (* "test_queen_move_invariant" >:: test_queen_move_invariant; *)
+         "test_king_can_move" >:: test_king_can_move;
+         (* "test_king_generate_moves" >:: test_king_generate_moves; *)
+         (* "test_king_move_invariant" >:: test_king_move_invariant; *)
        ]
 
 let board_tests = "board tests" >: test_list []
